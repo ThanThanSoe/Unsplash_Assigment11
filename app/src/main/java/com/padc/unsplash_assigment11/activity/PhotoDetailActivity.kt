3404a54af2c21a9a3c3823879da03e241aa68dd5
Than Than Soe <thanthansoe16@gmail.com>
2019-11-09 01:25:56 +0630

    second commit

--- app/src/main/java/com/padc/unsplash_assigment11/activity/PhotoDetailActivity.kt
+++ app/src/main/java/com/padc/unsplash_assigment11/activity/PhotoDetailActivity.kt
@@ -5,8 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.RelativeLayout
 import androidx.annotation.NonNull
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import coil.api.load
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -46,7 +46,7 @@ class PhotoDetailActivity: BaseActivity(), PhotoDetailView, PhotoListView {
     private lateinit var photoDetailPresenter: PhotoDetailPresenter
     private lateinit var photoListPresenter: PhotoListPresenter
     private lateinit var photoItemAdapter: PhotoItemAdapter
-    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<RelativeLayout>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
